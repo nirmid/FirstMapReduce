66ccb6cf52df9e25c06463fff2727e92ffe8c686
nir <nirmid@post.bgu.ac.il>
2022-12-21 21:23:46 +0200

    custom RecordReader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,14 +12,11 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Main {
 
-        public static class TokenizerMapper
-                extends Mapper<Object, Text, Text, IntWritable>{
-
+        public static class TokenizerMapper extends Mapper<Text, Text, Text, IntWritable>{
             private final static IntWritable one = new IntWritable(1);
             private Text word = new Text();
 
-            public void map(Object key, Text value, Context context
-            ) throws IOException, InterruptedException {
+            public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
                 StringTokenizer itr = new StringTokenizer(value.toString());
                 while (itr.hasMoreTokens()) {
                     word.set(itr.nextToken());
@@ -53,6 +50,7 @@ public class Main {
             job.setReducerClass(IntSumReducer.class);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(IntWritable.class);
+            job.setInputFormatClass(CustomInputFormat.class);
             FileInputFormat.addInputPath(job, new Path(args[0]));
             FileOutputFormat.setOutputPath(job, new Path(args[1]));
             System.exit(job.waitForCompletion(true) ? 0 : 1);
