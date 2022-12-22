cc096dfd128d9fc0fa0be88265c42d7a725ce65a
assiph <asif857@gmail.com>
2022-12-22 13:59:09 +0200

    working counting words

--- src/main/java/CustomRecordReader.java
+++ src/main/java/CustomRecordReader.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -13,16 +18,33 @@ public class CustomRecordReader extends RecordReader<Text,IntWritable> {
 	protected LineRecordReader reader;
 	protected Text key;
 	protected IntWritable value;
+	protected ArrayList<String> stopWords;
+
 
 	CustomRecordReader() {
 		reader = new LineRecordReader();
 		key = null;
 		value = null;
+		stopWords = new ArrayList<>();
 	}
-	
+	private void createStopWords(){
+		try {
+			File file = new File("stopwords.txt");
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] words = line.split(" ");
+				stopWords.addAll(Arrays.asList(words));
+			}
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	@Override
 	public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
 		reader.initialize(split, context);
+		createStopWords();
 	}
 
 
@@ -32,18 +54,40 @@ public class CustomRecordReader extends RecordReader<Text,IntWritable> {
 	}
 	
 	@Override
-	public boolean nextKeyValue() throws IOException, InterruptedException {
-		if (reader.nextKeyValue()) {
-			key = reader.getCurrentValue();
-			value = new IntWritable((int)reader.getCurrentKey().get()%2);
-			return true;
-		} else {
-			key = null;
-			value = null;
-			return false;
+	public boolean nextKeyValue() throws IOException{
+		while (true) {
+			if (reader.nextKeyValue()) {
+				key = reader.getCurrentValue();
+				transformKey();
+				value = new IntWritable((int) reader.getCurrentKey().get() % 2);
+				if (isKeyValid()) {
+					return true;
+				}
+			} else {
+				key = null;
+				value = null;
+				return false;
+			}
 		}
 	}
-	
+
+	private boolean isKeyValid() {
+		String[] keys = key.toString().split(",");
+		for (String strKey : keys){
+			if (stopWords.contains(strKey)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void transformKey() {
+		//int lengthWithoutLastBracket = key.getLength() - 1;
+		//String keyWithoutBrackets = key.toString().substring(1,lengthWithoutLastBracket);
+		String newKey = key.toString().replaceAll(" ", ",");
+		key.set(newKey);
+	}
+
 	@Override
 	public Text getCurrentKey() throws IOException, InterruptedException {
 		return key;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,34 +11,51 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Main {
         public static class TokenizerMapper
-                extends Mapper<Object, Text, Text, IntWritable>{
-
-            private final static IntWritable one = new IntWritable(1);
+                extends Mapper<Text, IntWritable, Text, IntWritable[]>{
+            private final static IntWritable[] countAll = {new IntWritable(1)};
+            private final static IntWritable[] addToCorpus0 = {new IntWritable(1),new IntWritable(0)};
+            private final static IntWritable[] addToCorpus1 = {new IntWritable(0),new IntWritable(1)};
             private Text word = new Text();
+            private final Text asterisk = new Text("*");
 
-            public void map(Object key, Text value, Context context
+            public void map(Text key, IntWritable value, Context context
             ) throws IOException, InterruptedException {
                 StringTokenizer itr = new StringTokenizer(value.toString());
                 while (itr.hasMoreTokens()) {
                     word.set(itr.nextToken());
-                    context.write(word, one);
+                    context.write(asterisk, countAll);
+                    if (value.get() == 1) {
+                        context.write(word, addToCorpus1);
+                    }
+                    else{
+                        context.write(word,addToCorpus0);
+                    }
                 }
             }
         }
 
         public static class IntSumReducer
-                extends Reducer<Text,IntWritable,Text,IntWritable> {
-            private IntWritable result = new IntWritable();
+                extends Reducer<Text,IntWritable[],Text,Text> {
 
-            public void reduce(Text key, Iterable<IntWritable> values,
+            public void reduce(Text key, Iterable<IntWritable[]> values,
                                Context context
             ) throws IOException, InterruptedException {
-                int sum = 0;
-                for (IntWritable val : values) {
-                    sum += val.get();
+                int sum0 = 0;
+                int sum1 = 0;
+                if (key.toString().equals("*")) {
+                    for (IntWritable[] val : values) {
+                        sum0 += val[0].get();
+                    }
+                    context.write(key,new Text(String.valueOf(sum0)));
+                } else {
+                    for (IntWritable[] val : values) {
+                        sum0 += val[0].get();
+                        sum1 += val[1].get();
+                    }
+                    Text result = new Text();
+                    result.set(sum0 + "," + sum1);
+                    context.write(key, result);
                 }
-                result.set(sum);
-                context.write(key, result);
             }
         }
 
@@ -54,8 +71,6 @@ public class Main {
             FileInputFormat.addInputPath(job, new Path(args[1]));
             FileOutputFormat.setOutputPath(job, new Path(args[2]));
             job.setInputFormatClass(CustomInputFormat.class);
-            FileInputFormat.addInputPath(job, new Path(args[0]));
-            FileOutputFormat.setOutputPath(job, new Path(args[1]));
             System.exit(job.waitForCompletion(true) ? 0 : 1);
         }
     }
